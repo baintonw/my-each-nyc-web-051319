429b5d3b1e313bd4b5cb1ebe24023efeca58e291
William Bainton <baintonw@gmail.com>
2019-05-04 18:55:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,9 @@ def my_each(collection)
 counter = 0
 yield
 end
-my_each(collection) while counter < collection.length |i|
+my_each(collection)
+  while counter < collection.length do |i|
   i
   counter += 1
+    end
 end
