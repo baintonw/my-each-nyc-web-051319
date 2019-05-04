111c0f1dc46045f1332ee944086a9f433d86c9f7
William Bainton <baintonw@gmail.com>
2019-05-04 18:26:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1, 2, 3, 4]
+def my_each(collection)
+counter = 0
+yield
+end
+my_each(collection) while counter < collection.length |i|
+  i
+  counter += 1
 end
