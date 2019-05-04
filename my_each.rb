146bc73713d1f3219905fb80d8dfb42c18ebf357
William Bainton <baintonw@gmail.com>
2019-05-04 19:09:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
 collection = [1, 2, 3, 4]
 
-def my_each(collection)
+def my_each(array)
   yield
 end
 
-my_each(collection) do |i|
+my_each(array) do |i|
  counter = 0
   while counter < collection.length
     i
